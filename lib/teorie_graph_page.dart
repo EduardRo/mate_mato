02bea13995d6170e977b759bc41c3cc5273b8156
EduardRo <eduardstancu@yahoo.com>
2025-02-12 00:06:02 +0200

    modif 120225 graph page

---
+++ lib/teorie_graph_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TeorieGraphPage extends StatefulWidget {
+  const TeorieGraphPage({super.key});
+
+  @override
+  State<TeorieGraphPage> createState() => TeorieGraphPageState();
+}
+
+class TeorieGraphPageState extends State<TeorieGraphPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
